e95d2069b47a34df876a6b3e99fe6ce279082150
siontama <siontama@naver.com>
2022-01-21 03:49:21 +0900

    index page

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var http = require('http');
 var app = express();
+var server = http.createServer(app);
+var path = require('path');
 
-app.listen(8080, function(){
+app.use(express.static('public'));
+
+var indexRouter = require('./routes/index');
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use('/',indexRouter);
+
+
+server.listen(8080, function(){
   console.log('Server 8080!');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.get('/', function(req, res, next) {
+	  res.render('views/index');
+});
 
 module.exports = router;
