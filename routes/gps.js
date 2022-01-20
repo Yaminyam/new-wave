37910c5f2de5de482c17e42ceac877ace9eaeb35
Hyewon-Koh <khw160929@jejunu.ac.kr>
2022-01-21 04:41:29 +0900

    map first

--- routes/gps.js
+++ routes/gps.js
@@ -1,4 +1,8 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
+
+router.get('/', function(req, res, next) {
+	  res.render('gps.html');
+});
 
 module.exports = router;
