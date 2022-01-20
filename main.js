a64883d0f41e59adf303f6ffff328d5eab7aadf9
siontama <siontama@naver.com>
2022-01-21 03:39:44 +0900

    init

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
-app.set('routes', 'ejs');
-
-app.get('/', function (req, res) {
-  res.render('index');
-});
-
 app.listen(8080, function(){
-  console.log('Main Page 8080!');
+  console.log('Server 8080!');
 });
