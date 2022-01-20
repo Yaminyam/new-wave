0784825faced3b0a31469d854bc70200e2bbca62
siontama <siontama@naver.com>
2022-01-21 04:02:54 +0900

    main

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
-var express = require('express');
-var http = require('http');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+
+var indexRouter = require("./routes/index");
+var gpsRouter = require("./routes/gps");
+
+const sequelize = require("./database");
+const session = require("express-session");
+var SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 var app = express();
-var server = http.createServer(app);
-var path = require('path');
 
-app.use(express.static('public'));
+sequelize.sync({}).then(() => console.log("db is ready"));
 
-var indexRouter = require('./routes/index');
-var gpsRouter = require('./routes/gps');
-
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 
-app.use('/',indexRouter);
-app.use('/gps',gpsRouter);
+app.use(cors());
 
-server.listen(8080, function(){
-  console.log('Server 8080!');
+app.use(
+  session({
+    secret: "keyboard cat",
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use("/", indexRouter);
+app.use("/gps", gpsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
