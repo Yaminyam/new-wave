24160d7fbf089548d8263373c057c47eff2b5735
Hyewon-Koh <khw160929@jejunu.ac.kr>
2022-01-20 23:46:23 +0900

    첫 시작!

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-//필요한 모듈 선언
 var express = require('express');
 var http = require('http');
 var app = express();
 
-//express 서버 포트 설정
-app.set('port', process.env.PORT || 8080);
+app.set('routes', 'ejs');
 
-//서버 생성
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
-//라우팅 모듈 선언
-//var indexRouter = require('./routes/index');
-
-//request 요청 URL과 처리 로직을 선언한 라우팅 모듈 매핑
-//app.use('/', indexRouter);
+app.listen(8080, function(){
+  console.log('Main Page 8080!');
+});
