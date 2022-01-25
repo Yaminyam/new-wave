9b067e24f2c550825f8b9ce462db2b498db4c287
siontama <siontama@naver.com>
2022-01-25 14:24:49 -0500

    chat room

--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var { v4: uuidV4 } = require("uuid");
 
-router.get("/", function (req, res, next) {
-	res.render("chat.html");
+router.get("/", (req, res) => {
+  res.redirect(`chat/${uuidV4()}`);
+});
+
+router.get("/:room", (req, res) => {
+  	res.render("chat.html", { roomId: req.params.room });
 });
 
 module.exports = router;
