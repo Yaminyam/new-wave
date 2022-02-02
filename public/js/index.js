66f40b546c8709f084fbe60bc8da4eab5dcc2367
siontama <siontama@naver.com>
2022-02-02 11:29:12 -0500

    chatlist

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 var messages = document.getElementById("msg");
 var gps = document.getElementById("map");
 var page = document.getElementById("page");
+var video = document.getElementById("video");
 
 messages.addEventListener("click", function (e) {
 	page.src = "/chat";
@@ -8,4 +9,8 @@ messages.addEventListener("click", function (e) {
 
 gps.addEventListener("click", function (e) {
 	page.src = "/gps";
+});
+
+video.addEventListener("click", function (e) {
+	page.src = "/video";
 });
