08778c5c9d982e86408ec13eba91d0ba9cb7175c
juhyun9 <96998861+juhyun9@users.noreply.github.com>
2022-02-08 08:09:41 +0900

    simple alarm
    그냥 작은 것으로 일단 했지만.... 점점 발전시키겠어요......

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,7 @@ var messages = document.getElementById('msg');
 var gps = document.getElementById('map');
 var page = document.getElementById('page');
 var video = document.getElementById('video');
+var Alarm = document.getElementById('Alarm');
 
 messages.addEventListener('click', function (e) {
 	page.src = '/chat';
@@ -14,3 +15,15 @@ gps.addEventListener('click', function (e) {
 video.addEventListener('click', function (e) {
 	page.src = '/video';
 });
+
+// 사용자 알림 권한 허용받기
+// Get user authorization permission 
+Notification.requestPermission();
+
+// 알림 내용
+//alarm content
+new Notification("Alarm", {body:'Someone needs help'});
+
+createRoom.addEventListener('click', function (e) {
+	new Notification("Alarm", {body:'Someone needs help'});
+  });
