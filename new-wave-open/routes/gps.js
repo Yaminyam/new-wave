26e62d4bd0a060aecf569fea7d10383665610ec1
juhyun9 <96998861+juhyun9@users.noreply.github.com>
2022-02-17 00:59:24 +0900

    delete new-wave-open

--- new-wave-open/routes/gps.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// Connect to gps.html
-router.get("/", function (req, res, next) {
-	  res.render("gps.html");
-});
-
-module.exports = router;
--- new-wave-open/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// Connect to index.html
-router.get('/', function(req, res, next) {
-	  res.render('index.html');
-});
-
-module.exports = router;
