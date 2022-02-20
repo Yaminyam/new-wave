6c67884fa40bd8e8f9dec0b252d58e3d87b6a8b9
siontama <siontama@naver.com>
2022-02-20 10:18:34 -0500

    gps

--- public/js/gps.js
+++ public/js/gps.js
@@ -6,19 +6,17 @@ var alarm = document.getElementById('alarm');
 
 var name = '';
 
-L.tileLayer(
-  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
-  {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: "mapbox/streets-v11",
-    maxZoom: 18,
-    tileSize: 512,
-    zoomOffset: -1,
-  }
-).addTo(map);
+L.tileLayer("/tiles/OSMPublicTransport/{z}/{x}/{y}.png", {
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: "mapbox/streets-v11",
+  maxZoom: 15,
+  minZoom: 13,
+  //tileSize: 512,
+  //zoomOffset: -1,
+}).addTo(map);
 
 // Perform when user connected
 socket.on('connect', function () {
